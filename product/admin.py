348f9ef04730c56926ea8e22230ddd11ce5575ca
Ne'matjon <118611615+codingboyuz@users.noreply.github.com>
2025-07-08 22:55:57 +0500

    detail page more image bilan ishlash lozim

--- product/admin.py
+++ product/admin.py
@@ -1,32 +1,50 @@
+# from django.contrib import admin
+# from django.contrib import admin
+# from django.utils.html import format_html
+# from .models import ProductModel
+#
+#
+#
+# @admin.register(ProductModel)
+# class ProductModelAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'name', 'description', 'category','image','created_at')
+#     list_display_links = ('id', 'name')
+#     search_fields = ('name', 'category')
+#     list_filter = ('name',)
+#     readonly_fields = ('image_preview',)
+#
+#     fieldsets = (
+#         (None, {
+#             'fields': ( 'name', 'description', 'category')
+#         }),
+#         ('Image', {
+#             'fields': ('image', 'image_preview'),
+#         }),
+#     )
+#
+#     def image_preview(self, obj):
+#         if obj.image and hasattr(obj.image, 'url'):
+#             return format_html('<img src="{}" style="max-height: 100px;" />', obj.image.url)
+#         return "-"
+#
+#
+#     image_preview.short_description = 'Image Preview'
+#
 from django.contrib import admin
-from django.contrib import admin
-from django.utils.html import format_html
-from .models import ProductModel
+from .models import ProductModel, ProductImage, ProductField
 
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+class ProductFieldInline(admin.TabularInline):
+    model = ProductField
+    extra = 1
 
 
 @admin.register(ProductModel)
 class ProductModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description', 'category','image','created_at')
-    list_display_links = ('id', 'name')
-    search_fields = ('name', 'category')
-    list_filter = ('name',)
-    readonly_fields = ('image_preview',)
-
-    fieldsets = (
-        (None, {
-            'fields': ( 'name', 'description', 'category')
-        }),
-        ('Image', {
-            'fields': ('image', 'image_preview'),
-        }),
-    )
-
-    def image_preview(self, obj):
-        if obj.image and hasattr(obj.image, 'url'):
-            return format_html('<img src="{}" style="max-height: 100px;" />', obj.image.url)
-        return "-"
-    
-    
-    image_preview.short_description = 'Image Preview'
-
+    list_display = ['name', 'category', 'created_at']
+    inlines = [ProductImageInline, ProductFieldInline]
--- product/views.py
+++ product/views.py
@@ -1,60 +1,105 @@
-from django.views import View
-from django.shortcuts import render
-from .models import ProductModel
-import logging
-from django.core.paginator import Paginator
-
-logger = logging.getLogger(__name__)
-
-# class ProductsPage(View):
+# from django.views import View
+# from django.shortcuts import render
+# from .models import ProductModel
+# import logging
+# from django.core.paginator import Paginator
+#
+# logger = logging.getLogger(__name__)
+#
+# # class ProductsPage(View):
+# #     def get(self, request):
+# #         try:
+# #             data = ProductModel.objects.all()
+# #             categories = ["All Products", "Vehicles", "Communication Systems", "Protective Gear"]
+# #             product = list(data)*20
+# #             return render(request, "products.html", {
+# #                 "data": product,
+# #                 "categories": categories
+# #             })
+# #         except Exception as e:
+# #             logger.error(f"Home sahifasida xatolik: {str(e)}")
+# #             return render(request, "error.html", {"message": "Noma'lum xatolik yuz berdi."})
+#
+#
+# # class ProductsPage(View):
+# #     def get(self,request):
+# #         data  =ProductModel.objects.all().order_by('id')
+# #         product = list(data)*100
+# #         search_query = request.GET.get('q')
+#
+# #         if search_query:
+# #             product=product.filter(title__icontais=search_query)
+#
+# #         page_size = request.GET.get('page_size', 8)
+# #         paginator = Paginator(product, page_size)
+#
+# #         page_num = request.GET.get('page', 1)
+# #         pages = paginator.get_page(page_num)
+# #         categories = ["All Products", "Vehicles", "Communication Systems", "Protective Gear"]
+# #         context = {'pages': pages,"categories": categories}
+# #         return render(request=request,template_name='products.html',context=context)
+#
+#
+#
+# from django.views import View
+# from django.core.paginator import Paginator
+# from django.shortcuts import render
+# from .models import ProductModel
+#
+# class ProductsView(View):
 #     def get(self, request):
-#         try:
-#             data = ProductModel.objects.all()
-#             categories = ["All Products", "Vehicles", "Communication Systems", "Protective Gear"]
-#             product = list(data)*20
-#             return render(request, "products.html", {
-#                 "data": product,
-#                 "categories": categories
-#             })
-#         except Exception as e:
-#             logger.error(f"Home sahifasida xatolik: {str(e)}")
-#             return render(request, "error.html", {"message": "Noma'lum xatolik yuz berdi."})
-
-
-# class ProductsPage(View):
-#     def get(self,request):
-#         data  =ProductModel.objects.all().order_by('id')
-#         product = list(data)*100
+#         data = ProductModel.objects.all().order_by('id')
+#         # product = list(data) * 100
+#
 #         search_query = request.GET.get('q')
-
 #         if search_query:
-#             product=product.filter(title__icontais=search_query)
-        
-#         page_size = request.GET.get('page_size', 8)
-#         paginator = Paginator(product, page_size)
-
+#             data = data.filter(name__icontains=search_query)
+#             product = list(data)
+#
+#         page_size = int(request.GET.get('page_size', 10))
+#         paginator = Paginator(data, page_size)
+#
 #         page_num = request.GET.get('page', 1)
 #         pages = paginator.get_page(page_num)
+#
+#         # 👉 Custom page range with ellipsis
+#         page_range = paginator.get_elided_page_range(
+#             number=pages.number,
+#             on_each_side=1,
+#             on_ends=2
+#         )
+#
 #         categories = ["All Products", "Vehicles", "Communication Systems", "Protective Gear"]
-#         context = {'pages': pages,"categories": categories}
-#         return render(request=request,template_name='products.html',context=context)
-
-
-
+#
+#         context = {
+#             'pages': pages,
+#             'page_range': page_range,  # qo‘shildi
+#             "categories": categories
+#         }
+#         return render(request, 'products.html', context)
+#
+#
+#
+# class ProductsDetailView(View):
+#     def get(self, request, pk):
+#         product = ProductModel.objects.all().get(pk=pk)
+#
+#         context = {
+#             'product': product,
+#         }
+#         return render(request, 'product_detail.html', context)
 from django.views import View
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-from django.shortcuts import render
 from .models import ProductModel
 
 class ProductsView(View):
     def get(self, request):
         data = ProductModel.objects.all().order_by('id')
-        # product = list(data) * 100
 
         search_query = request.GET.get('q')
         if search_query:
             data = data.filter(name__icontains=search_query)
-            product = list(data)
 
         page_size = int(request.GET.get('page_size', 10))
         paginator = Paginator(data, page_size)
@@ -62,7 +107,7 @@ class ProductsView(View):
         page_num = request.GET.get('page', 1)
         pages = paginator.get_page(page_num)
 
-        # 👉 Custom page range with ellipsis
+        # Sahifalash uchun elided page range (… bilan qisqartirilgan raqamlar)
         page_range = paginator.get_elided_page_range(
             number=pages.number,
             on_each_side=1,
@@ -73,18 +118,22 @@ class ProductsView(View):
 
         context = {
             'pages': pages,
-            'page_range': page_range,  # qo‘shildi
+            'page_range': page_range,
             "categories": categories
         }
         return render(request, 'products.html', context)
 
 
-
 class ProductsDetailView(View):
     def get(self, request, pk):
-        product = ProductModel.objects.all().get(pk=pk)
+        product = get_object_or_404(ProductModel, pk=pk)
+
+        images = product.images.all()  # ProductImage modelidagi rasmlar
+        fields = product.fields.all()  # ProductField modelidagi qo‘shimcha maydonlar
 
         context = {
             'product': product,
+            'images': images,
+            'fields': fields
         }
         return render(request, 'product_detail.html', context)
