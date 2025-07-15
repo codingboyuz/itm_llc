eb7cbe27bcbcc21e42e6d70403261d363d7a262b
codingboyuz <118611615+codingboyuz@users.noreply.github.com>
2025-07-15 15:06:44 +0500

    base.html o'zgartishlar qilindi

--- core/settings.py
+++ core/settings.py
@@ -1,24 +1,16 @@
-
+import os
 from pathlib import Path
 from decouple import config
-from django import template
-import os
+from django.utils.translation import gettext_lazy as _
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',default=False,cast=bool)
-
-ALLOWED_HOSTS = ["itm-llc.uz","www.itm-llc.uz"]
-
+ALLOWED_HOSTS = ["itm-llc.uz", "www.itm-llc.uz"]
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -26,34 +18,32 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # package
+
+    # 3rd-party
     'modeltranslation',
-    'whitenoise',
+    'whitenoise.runserver_nostatic',
     'corsheaders',
 
-    # local app
-    "home",
-    'product.apps.ProductConfig', 
-    "news",
+    # Local apps
+    'home',
+    'product.apps.ProductConfig',
+    'news',
     'about.apps.AboutConfig',
-    'shared'
+    'shared',
 ]
-
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # WhiteNoise middleware yuqorida bo'lishi kerak
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    "django.middleware.locale.LocaleMiddleware",
-    'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core.middleware.request_logger.RequestLoggerMiddleware',  # 👈 bu yerda,
+    'core.middleware.request_logger.RequestLoggerMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -68,6 +58,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.static_version',
             ],
         },
     },
@@ -75,19 +66,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# SECURE_BROWSER_XSS_FILTER = True
-# SECURE_HSTS_SECONDS = 31536000
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
+# CORS
 CORS_ALLOW_ALL_ORIGINS = True
-
 CORS_ALLOWED_ORIGINS = [
     "https://itm-llc.uz",
 ]
 
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Logging
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -122,7 +115,6 @@ LOGGING = {
             'filename': BASE_DIR / 'logs/about.log',
             'formatter': 'verbose',
         },
-
         'news_file': {
             'level': 'INFO',
             'class': 'logging.FileHandler',
@@ -137,120 +129,56 @@ LOGGING = {
         },
     },
     'loggers': {
-        'home': {
-            'handlers': ['home_file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        'product': {
-            'handlers': ['product_file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        'product_detail_file': {
-            'handlers': ['product_detail_file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        'about': {
-            'handlers': ['about_file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-         'news': {
-            'handlers': ['news_file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        'news-detail': {
-            'handlers': ['news_detail_file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
+        'home': {'handlers': ['home_file'], 'level': 'INFO', 'propagate': False},
+        'product': {'handlers': ['product_file'], 'level': 'INFO', 'propagate': False},
+        'product_detail': {'handlers': ['product_detail_file'], 'level': 'INFO', 'propagate': False},
+        'about': {'handlers': ['about_file'], 'level': 'INFO', 'propagate': False},
+        'news': {'handlers': ['news_file'], 'level': 'INFO', 'propagate': False},
+        'news-detail': {'handlers': ['news_detail_file'], 'level': 'INFO', 'propagate': False},
     }
 }
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
-
 
 # Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
-LANGUAGE_CODE = 'uz'  # ✅ shu bo'lishi kerak
-
-
+LANGUAGE_CODE = 'uz'
 TIME_ZONE = 'Asia/Tashkent'
-
 USE_I18N = True
-
 USE_TZ = True
 
-from django.utils.translation import gettext_lazy as _
-
 LANGUAGES = [
-    ('uz',_('Uzbek')),
-    ('en',_('English')),
-    ('ru',_('Russian')),
+    ('uz', _('Uzbek')),
+    ('en', _('English')),
+    ('ru', _('Russian')),
 ]
-
 
 MODELTRANSLATION_DEFAULT_LANGUAGE = 'uz'
 MODELTRANSLATION_PREPOPULATE_LANGUAGE = 'uz'
 
-LOCALE_PATHS = BASE_DIR, 'locale'
-
-
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-STATIC_URL = '/static/'
-
-
-SSTATICFILES_DIRS = [
-    BASE_DIR / "static",
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# Static & media
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = '/home/itmllcuz/itm-llc/media'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
